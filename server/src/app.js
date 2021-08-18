0d119947bd3d15cf2c97633c37a0b47bd499b93b
Zeapherine <zeapherine@gmail.com>
2021-08-19 01:14:08 +0530

    added deploy script - to build and serve react production build files as static files from the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+
 const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
@@ -10,7 +12,13 @@ app.use(
 		origin: 'http://localhost:3000',
 	})
 );
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
