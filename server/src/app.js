afcdd02b3583eb01c39a07c878a36b9301ef2b18
Zeapherine <zeapherine@gmail.com>
2021-08-19 23:54:32 +0530

    added GET - /launches routes

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(planetsRouter);
+app.use(launchesRouter);
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
