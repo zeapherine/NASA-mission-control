c94b9f384c88cd5f63967ddf5856e7791259c6e6
Zeapherine <zeapherine@gmail.com>
2021-08-20 13:40:22 +0530

    added DELETE routes to abort missions

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
+const {
+	getAllLaunches,
+	addNewLaunch,
+	existsLaunchWithId,
+	abortLaunchById,
+} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
 	return res.status(200).json(getAllLaunches());
@@ -25,7 +30,20 @@ function httpAddNewLaunch(req, res) {
 	return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+	const launchId = Number(req.params.id);
+
+	// if launch doesn't exist
+	if (!existsLaunchWithId(launchId)) {
+		return res.status(404).json({ error: 'Launch not found' });
+	}
+
+	const aborted = abortLaunchById(launchId);
+	return res.status(200).json(aborted);
+}
+
 module.exports = {
 	httpGetAllLaunches,
 	httpAddNewLaunch,
+	httpAbortLaunch,
 };
