8117dff7ce63c5ff39840cfb81712f884b50c541
Zeapherine <zeapherine@gmail.com>
2021-08-20 12:48:47 +0530

    added POST /lauches on FE, and edited launch model to match frontend naming

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -15,9 +15,21 @@ async function httpGetLaunches() {
 	});
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-	// TODO: Once API is ready.
-	// Submit given launch data to launch system.
+	try {
+		return await fetch(`${API_URL}/launches`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(launch), // body needs to be a string
+		});
+	} catch (e) {
+		return {
+			ok: false,
+		};
+	}
 }
 
 async function httpAbortLaunch(id) {
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
 	mission: 'Kepler Exploration X',
 	rocket: 'Explore IS1',
 	launchDate: new Date('December 27, 2030'),
-	destination: 'Kepler-442 b',
+	target: 'Kepler-442 b',
 	customer: ['NASA', 'NOAA'],
 	upcoming: true,
 	success: true,
