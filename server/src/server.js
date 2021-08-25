87eeb39768e8a0b8915a117d2b7360e64fc43926
Zeapherine <zeapherine@gmail.com>
2021-08-25 20:58:51 +0530

    added mongoose connection to mongoDB atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,30 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+dotenv.config();
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+	console.log('MongoDB connection is ready');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.error(err);
+});
+
 async function startServer() {
+	await mongoose.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
 	await loadPlanetsData();
 
 	server.listen(PORT, () => {
