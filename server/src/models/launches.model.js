8e0f040808f00a99712b098cbb6ff69f978f1dba
Zeapherine <zeapherine@gmail.com>
2021-08-26 00:08:55 +0530

    added Referential Integrity -> check if planets exists

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,4 +1,5 @@
 const launchesDatabase = require('./launches.mongo');
+const planets = require('./planets.mongo');
 
 const launches = new Map();
 
@@ -32,6 +33,14 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
+	const planet = await planets.findOne({
+		keplerName: launch.target,
+	});
+
+	if (!planet) {
+		throw new Error('No matching planet was found');
+	}
+
 	await launchesDatabase.updateOne(
 		{
 			// if it exists update else insert
