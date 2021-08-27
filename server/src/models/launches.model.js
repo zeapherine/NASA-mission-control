b693ce685d5eb18f0fafcfc24290f79741050b53
Zeapherine <zeapherine@gmail.com>
2021-08-27 23:41:59 +0530

    aborting launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -18,8 +18,10 @@ const launch = {
 
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-	return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+	return await launchesDatabase.findOne({
+		flightNumber: launchId,
+	});
 }
 
 async function getLatestFlightNumber() {
@@ -52,7 +54,7 @@ async function saveLaunch(launch) {
 		throw new Error('No matching planet was found');
 	}
 
-	await launchesDatabase.updateOne(
+	await launchesDatabase.findOneAndUpdate(
 		{
 			// if it exists update else insert
 			flightNumber: launch.flightNumber,
@@ -77,11 +79,19 @@ async function scheduleNewLaunch(launch) {
 	await saveLaunch(newLaunch);
 }
 
-function abortLaunchById(launchId) {
-	const aborted = launches.get(launchId);
-	aborted.upcoming = false;
-	aborted.success = false;
-	return aborted;
+async function abortLaunchById(launchId) {
+	const aborted = await launchesDatabase.updateOne(
+		{
+			flightNumber: launchId,
+		},
+		{
+			upcoming: false,
+			success: false,
+		}
+	);
+	console.log(aborted);
+
+	return aborted.acknowledged === true && aborted.modifiedCount === 1;
 }
 
 module.exports = {
