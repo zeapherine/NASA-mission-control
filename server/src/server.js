33500bf959f6c28c81481448a0772a820cc5b244
Zeapherine <zeapherine@gmail.com>
2021-08-29 22:01:04 +0530

    updated test

--- server/src/server.js
+++ server/src/server.js
@@ -1,34 +1,21 @@
 const http = require('http');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
-dotenv.config();
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-	console.log('MongoDB connection is ready');
-});
-
-mongoose.connection.on('error', (err) => {
-	console.error(err);
-});
-
 async function startServer() {
 	try {
-		await mongoose.connect(process.env.MONGO_URL);
-
+		await mongoConnect();
 		await loadPlanetsData();
 
 		server.listen(PORT, () => {
 			console.log(`Listening on port ${PORT}....`);
 		});
-
 	} catch (e) {
 		console.log(e);
 	}
---
+++ server/src/services/mongo.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connection.once('open', () => {
+	console.log('MongoDB connection is ready');
+});
+
+mongoose.connection.on('error', (err) => {
+	console.error(err);
+});
+
+async function mongoConnect() {
+	await mongoose.connect(process.env.MONGO_URL);
+}
+
+async function mongoDisconnect() {
+	await mongoose.disconnect();
+}
+
+module.exports = {
+	mongoConnect,
+	mongoDisconnect,
+};
